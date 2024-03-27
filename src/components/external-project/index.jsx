ec0c4557ea5a47b859a1ec83626cef64ea5b14f2
SirajunNasihin <35190761+Sirajunnasihin@users.noreply.github.com>
2024-03-27 13:22:55 +0800

    Update index.jsx

--- src/components/external-project/index.jsx
+++ src/components/external-project/index.jsx
@@ -132,7 +132,7 @@ const ExternalProject = ({ externalProjects, loading, googleAnalytics }) => {
     <Fragment>
       {displaySection(externalProjects) && (
         <div className="col-span-1 lg:col-span-2">
-          <div className="grid grid-cols-4 gap-2">
+          <div className="grid grid-cols-2 gap-2">
             <div className="col-span-2">
               <div className="card compact bg-base-100 shadow bg-opacity-40">
                 <div className="card-body">
