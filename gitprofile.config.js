81c0b47b4a2c9d446a06d848d5b9d4216640e549
SirajunNasihin <35190761+Sirajunnasihin@users.noreply.github.com>
2024-03-27 13:03:06 +0800

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -7,7 +7,7 @@ const config = {
     limit: 6, // How many projects to display.
     exclude: {
       forks: false, // Forked projects will not be displayed if set to true.
-      projects: [], // These projects will not be displayed. example: ['my-project1', 'my-project2']
+      projects: ['voyager', 'portfolio', 'exam', 'sky-cms-package'], // These projects will not be displayed. example: ['my-project1', 'my-project2']
     },
   },
   social: {
